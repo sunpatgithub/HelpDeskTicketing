6d64b9f1bcb0ab8eb2caf9cebc07ee4d96945abf
Sunil Patel <65399096+sunpatgithub@users.noreply.github.com>
2020-05-27 18:08:48 +0530

    Add files via upload

---
+++ HR.WebApi/Model/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HR.WebApi.Model
+{
+    public class Category
+    {
+        [Key]
+        public int CatId { get; set; }
+        public string CatName { get; set; }
+        public Int16 isActive { get; set; }
+        public int? AddedBy { get; set; }
+        public DateTime? AddedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
