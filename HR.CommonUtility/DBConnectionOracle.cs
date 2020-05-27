54a960c97372be2b51bb47b87aeef4fc2b7dedf1
Sunil Patel <65399096+sunpatgithub@users.noreply.github.com>
2020-05-27 22:25:31 +0530

    testing

--- HR.CommonUtility/DBConnectionOracle.cs
+++ HR.CommonUtility/DBConnectionOracle.cs
@@ -9,7 +9,7 @@ namespace HR.CommonUtility
     {
         public string Connect()
         {
-            return "server=172.16.2.129;port=3306;database=hr_proj;user=root;password=root2233;SslMode=None;";
+            return "server=172.16.2.129;port=3306;database=helpdesk;user=root;password=root2233;SslMode=None;";
         }
 
         public int ConnectionTimeout()
