07fb1ac4f36ea6b1a54d6d128615f7f7313a7600
Sunil Patel <65399096+sunpatgithub@users.noreply.github.com>
2020-05-28 15:46:51 +0530

    Made changes in Startup.cs

--- HR.WebApi/Startup.cs
+++ HR.WebApi/Startup.cs
@@ -178,6 +178,9 @@ namespace HR.WebApi
 
             #region Help Desk Repository List
             services.AddTransient(typeof(ICommonRepository<Category>), typeof(CategoryRepository<Category>));
+            services.AddTransient(typeof(ICommonRepository<Ticket>), typeof(TicketRepository<Ticket>));
+            services.AddTransient(typeof(ICommonRepository<TicketLog>), typeof(TicketRepository<TicketLog>));
+            //services.AddTransient(typeof(ICommonRepository<Ticket>), typeof(TicketRepository<Ticket>));
             #endregion
 
 
