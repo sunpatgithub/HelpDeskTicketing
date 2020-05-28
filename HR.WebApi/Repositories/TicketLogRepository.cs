9dec5c401d23799fdb3768644b7da93bb08cd769
Sunil Patel <65399096+sunpatgithub@users.noreply.github.com>
2020-05-28 15:45:38 +0530

    Added ticket and ticketlog repositories

---
+++ HR.WebApi/Repositories/TicketLogRepository.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.WebApi.DAL;
+using HR.WebApi.Exceptions;
+using HR.WebApi.Interfaces;
+using HR.WebApi.Model;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+
+namespace HR.WebApi.Repositories
+{
+    public class TicketLogRepository<T> 
+    {
+        private readonly ApplicationDbContext adbContext;
+
+        public TicketLogRepository(ApplicationDbContext applicationDbContext)
+        {
+            adbContext = applicationDbContext;
+        }
+
+        public async Task<IEnumerable<TicketLog>> GetAll(int RecordLimit)
+        {
+            try
+            {
+                IEnumerable<TicketLog> vList;
+                if (RecordLimit > 0)
+                    vList = adbContext.TicketLog.Take(RecordLimit).ToList();
+                else
+                    vList = adbContext.TicketLog.ToList();
+                if (vList == null || vList.Count() == 0)
+                    throw new RecoredNotFoundException("Get Data Empty");
+
+                return await Task.FromResult(vList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TicketLog>> Get(int id)
+        {
+            try
+            {
+                var vList = adbContext.TicketLog.Where(w => w.LogId == id).ToList();
+                if (vList == null || vList.Count() == 0)
+                    throw new RecoredNotFoundException("Get Data Empty");
+
+                return await Task.FromResult(vList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public async Task<IEnumerable<TicketLog>> FindPaginated(int pageIndex, int pageSize, string searchValue)
+        //{
+        //    try
+        //    {
+        //        IEnumerable<TicketLog> vList;
+        //        if (String.IsNullOrEmpty(searchValue))
+        //            vList = adbContext.ticketlog.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        //        else
+        //            vList = adbContext.ticketlog.Where(w => new[] { w.Action.ToLower() }.Any(a => a.Contains(searchValue.ToLower()))).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        //        if (vList == null || vList.Count() == 0)
+        //            throw new RecoredNotFoundException("Get Data Empty");
+
+        //        return await Task.FromResult(vList);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        public async Task Insert(TicketLog entity)
+        {
+            try
+            {
+                entity.AddedOn = DateTime.Now;
+                adbContext.TicketLog.Add(entity);
+                await Task.FromResult(adbContext.SaveChanges());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public async Task Update(TicketLog entity)
+        //{
+        //    try
+        //    {
+        //        try
+        //        {
+        //            var lstTicketLog = adbContext.ticketlog.Where(x => x.LogId == entity.LogId).FirstOrDefault();
+        //            if (lstTicketLog == null)
+        //                throw new RecoredNotFoundException("Data Not Available");
+        //            lstTicketLog.TicketId = entity.TicketId;
+        //            lstTicketLog.Action = entity.Action;
+        //            lstTicketLog.Comments = entity.Comments;
+        //            //lstTicketLog.isActive = entity.isActive;
+        //            lstTicketLog.AddedBy = entity.AddedBy;
+        //            lstTicketLog.AddedOn = entity.AddedOn;
+        //            lstTicketLog.UpdatedBy = entity.Up
+        //            lstTicketLog.UpdatedOn = DateTime.Now;
+
+        //            adbContext.category.Update(lstCategory);
+        //            await Task.FromResult(adbContext.SaveChanges());
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            throw ex;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //public async Task ToogleStatus(int id, Int16 isActive)
+        //{
+        //    try
+        //    {
+        //        //update flag isActive=0
+        //        var vList = adbContext.category.Where(w => w.CatId == id && w.isActive != isActive).SingleOrDefault();
+        //        if (vList == null)
+        //            throw new RecoredNotFoundException("Data Not Available");
+        //        vList.isActive = isActive;
+        //        adbContext.category.Update(vList);
+        //        await Task.FromResult(adbContext.SaveChanges());
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //public async Task Delete(int id)
+        //{
+        //    try
+        //    {
+        //        //Delete Category
+        //        var vList = adbContext.category.Where(w => w.CatId == id).ToList().SingleOrDefault();
+        //        if (vList == null)
+        //            throw new RecoredNotFoundException("Data Not Available");
+        //        adbContext.category.Remove(vList);
+        //        await Task.FromResult(adbContext.SaveChanges());
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //public bool Exists(TicketLog entity)
+        //{
+        //    try
+        //    {
+        //        int intCount = 0;
+        //        if (entity.CatId > 0)
+        //            intCount = adbContext.category.Where(w => w.CatId != entity.CatId && (w.CatName == entity.CatName)).Count();
+        //        else
+        //            intCount = adbContext.category.Where(w => w.CatName == entity.CatName).Count();
+        //        return (intCount > 0 ? true : false);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //public int RecordCount(string searchValue)
+        //{
+        //    try
+        //    {
+        //        if (String.IsNullOrEmpty(searchValue))
+        //        {
+        //            //Find Category all no of rows
+        //            var vCount = adbContext.category.Count();
+        //            return vCount;
+        //        }
+        //        else
+        //        {
+        //            //Find Category no of rows with Searching
+        //            var vCount = adbContext.category.Where(w => new[] { w.CatName.ToLower() }.Any(a => a.Contains(searchValue.ToLower()))).Count();
+        //            return vCount;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
---
+++ HR.WebApi/Repositories/TicketRepository.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR.WebApi.DAL;
+using HR.WebApi.Exceptions;
+using HR.WebApi.Interfaces;
+using HR.WebApi.Model;
+
+namespace HR.WebApi.Repositories
+{
+    public class TicketRepository<T> : ICommonRepository<Ticket>
+    {
+        private readonly ApplicationDbContext adbContext;
+
+        public TicketRepository(ApplicationDbContext applicationDbContext)
+        {
+            adbContext = applicationDbContext;
+        }
+
+        public async Task<IEnumerable<Ticket>> GetAll(int RecordLimit)
+        {
+            try
+            {
+                IEnumerable<Ticket> vList;
+                if (RecordLimit > 0)
+                    vList = adbContext.Ticket.Take(RecordLimit).ToList();
+                else
+                    vList = adbContext.Ticket.ToList();
+                if (vList == null || vList.Count() == 0)
+                    throw new RecoredNotFoundException("Get Data Empty");
+
+                return await Task.FromResult(vList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<Ticket>> Get(int id)
+        {
+            try
+            {
+                var vList = adbContext.Ticket.Where(w => w.TicketId == id).ToList();
+                if (vList == null || vList.Count() == 0)
+                    throw new RecoredNotFoundException("Get Data Empty");
+
+                return await Task.FromResult(vList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<Ticket>> FindPaginated(int pageIndex, int pageSize, string searchValue)
+        {
+            try
+            {
+                IEnumerable<Ticket> vList;
+                if (String.IsNullOrEmpty(searchValue))
+                    vList = adbContext.Ticket.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+                else
+                    vList = adbContext.Ticket.Where(w => new[] { w.Subject.ToLower() }.Any(a => a.Contains(searchValue.ToLower()))).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+                if (vList == null || vList.Count() == 0)
+                    throw new RecoredNotFoundException("Get Data Empty");
+
+                return await Task.FromResult(vList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Insert(Ticket entity)
+        {
+            try
+            {
+                entity.AddedOn = DateTime.Now;
+                adbContext.Ticket.Add(entity);
+                await Task.FromResult(adbContext.SaveChanges());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Update(Ticket entity)
+        {
+            try
+            {
+                try
+                {
+                    var lstTicket = adbContext.Ticket.Where(x => x.TicketId == entity.TicketId).FirstOrDefault();
+                    if (lstTicket == null)
+                        throw new RecoredNotFoundException("Data Not Available");
+                    lstTicket.RequesterId = entity.RequesterId;
+                    lstTicket.Subject = entity.Subject;
+                    lstTicket.Description = entity.Description;
+                    lstTicket.CatId = entity.CatId;
+                    lstTicket.Status = entity.Status;
+                    lstTicket.Priority = entity.Priority;
+                    lstTicket.DeptId = entity.DeptId;
+                    lstTicket.AssigneeId = entity.AssigneeId;
+                    lstTicket.IsActive = entity.IsActive;
+                    lstTicket.AddedBy = entity.AddedBy;
+                    lstTicket.AddedOn = entity.AddedOn;
+                    lstTicket.UpdatedBy = entity.UpdatedBy;
+                    lstTicket.UpdatedOn = DateTime.Now;
+                    
+
+                    adbContext.Ticket.Update(lstTicket);
+                    await Task.FromResult(adbContext.SaveChanges());
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task ToogleStatus(int id, Int16 isActive)
+        {
+            try
+            {
+                //update flag isActive=0
+                var vList = adbContext.Ticket.Where(w => w.TicketId == id && w.IsActive != isActive).SingleOrDefault();
+                if (vList == null)
+                    throw new RecoredNotFoundException("Data Not Available");
+                vList.IsActive = isActive;
+                adbContext.Ticket.Update(vList);
+                await Task.FromResult(adbContext.SaveChanges());
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                //Delete Category
+                var vList = adbContext.Ticket.Where(w => w.TicketId == id).ToList().SingleOrDefault();
+                if (vList == null)
+                    throw new RecoredNotFoundException("Data Not Available");
+                adbContext.Ticket.Remove(vList);
+                await Task.FromResult(adbContext.SaveChanges());
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Exists(Ticket entity)
+        {
+            try
+            {
+                int intCount = 0;
+                if (entity.TicketId > 0)
+                    intCount = adbContext.Ticket.Where(w => w.TicketId != entity.TicketId && (w.Subject == entity.Subject)).Count();
+                else
+                    intCount = adbContext.Ticket.Where(w => w.Subject == entity.Subject).Count();
+                return (intCount > 0 ? true : false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int RecordCount(string searchValue)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(searchValue))
+                {
+                    //Find Category all no of rows
+                    var vCount = adbContext.Ticket.Count();
+                    return vCount;
+                }
+                else
+                {
+                    //Find Category no of rows with Searching
+                    var vCount = adbContext.Ticket.Where(w => new[] { w.Subject.ToLower() }.Any(a => a.Contains(searchValue.ToLower()))).Count();
+                    return vCount;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
